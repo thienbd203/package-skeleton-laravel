709b64a65915020878d567f9fa22459c5a099f3e
thienbd <thienbd@absoltech.vn>
2025-11-25 10:29:12 +0700

    refactor(table): expand actions return type to support file downloads and responses
    Add BinaryFileResponse and Response to TableContract actions method return type to support file downloads and custom HTTP responses in addition to redirects.

--- src/Inertia/Concerns/TableContract.php
+++ src/Inertia/Concerns/TableContract.php
@@ -3,11 +3,13 @@
 namespace InertiaKit\InertiaTableBuilder\Inertia\Concerns;
 
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use InertiaKit\InertiaTableBuilder\Inertia\Tables\Table;
 
 interface TableContract
 {
     public static function build(): Table;
 
-    public static function actions(): RedirectResponse;
+    public static function actions(): RedirectResponse|BinaryFileResponse|Response;
 }
